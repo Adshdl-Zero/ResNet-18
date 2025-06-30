30c315bafb3ddbcaa74815e3a5a524f153bf3d49
Adshdl-Zero <adshdlzero@gmail.com>
2025-06-30 14:16:46 +0530

    Add the trained model

---
+++ train.py
@@ -0,0 +1,28 @@
+import tensorflow as tf
+from models.resnet import res_net
+from data.dataset import load_cifar
+from utils.callbacks import cosine_decay_scheduler, get_tensorboard_callback
+
+(x_train, y_train), (x_test, y_test), augment = load_cifar()
+
+model = res_net()
+
+model.compile(optamizer =  tf.keras.optimizers.Adam(learning_rate=0.1),
+              loss = 'spase_categorical_crossentropy',
+              metrics = ['accuracy']
+              )
+
+callbacks = [
+    cosine_decay_scheduler(100, 0.1),
+    get_tensorboard_callback()
+]
+
+model.train(
+    augment(x_train), y_train,
+    validation_split = 0.1,
+    epoch = 100,
+    batch_size = 128,
+    callbacks = callbacks
+)
+
+model.save('resnet18_cifar10.h5')
